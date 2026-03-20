19b575025819f8b91e0b604ec8a7ce37e7f728de
ChillibyteUK <dan@chillibyte.co.uk>
2026-03-20 11:16:42 +0000

    text page

--- page-templates/text-page.php
+++ page-templates/text-page.php
@@ -10,18 +10,11 @@ get_header();
 
 ?>
 <main id="main" class="text-page">
-	<div class="has-lime-1000-border-top has-lime-1000-border-bottom mt-4">
-		<h1 class="id-container px-4 px-md-5 fs-800 fw-light has-lime-1100-color lh-tightest pt-2 pb-1"><?= esc_html( get_the_title() ); ?></h1>
-	</div>
-	<div class="id-container">
-		<div class="row post-content-row mb-5">
-			<div class="col-md-3"></div>
-			<div class="col-md-9 post-content px-4 px-md-5 ps-md-0 pe-md-5">
-				<?php
-				echo apply_filters( 'the_content', get_the_content() ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-				?>
-			</div>
-		</div>
+	<div class="container">
+		<h1><?= esc_html( get_the_title() ); ?></h1>
+		<?php
+		echo apply_filters( 'the_content', get_the_content() ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		?>
 	</div>
 </main>
 <?php
